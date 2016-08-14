4faa6a99174c1649105320088b06b040761e0fd7
Vov4egk <kornienko90@gmail.com>
2016-08-14 13:05:14 +0300

    module1 homework

---
+++ src/FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass{
+    public static void main(String[] args) {
+        System.out.println("My first programs on Java...");
+        //здесть слово first специально написано с буковкой R
+        // в остальных классах - строго как на слайде
+    }
+}
---
+++ src/Fourth.java
@@ -0,0 +1,5 @@
+public class Fourth {
+    public static void main(String[] args) {
+        System.out.println("My fist programs on Java...");
+    }
+}
