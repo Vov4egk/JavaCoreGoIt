318ad5964d803fd394293763b5a4617e8597ece1
Vov4egk <kornienko90@gmail.com>
2016-08-21 17:11:50 +0300

    package name

--- src/Module1/FirstClass.java
+++ src/Module1/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass{
         System.out.println("My first programs on Java...");
         //Here the word fiRst deliberately written with the letter r
         //in other classes "fist" - just like on a slide in a presentation
+        // now I try to commit this package with gitignore
     }
 }
