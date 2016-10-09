dcc267f50c3f28e7167231765b04178b62eb2ffc
Vov4egk <kornienko90@gmail.com>
2016-10-09 20:47:33 +0300

    task1

---
+++ src/Module3/homework/Bird.java
@@ -0,0 +1,11 @@
+package Module3.homework;
+
+public class Bird {
+  void sing(){
+      System.out.println(
+              "I am walking\n" +
+              "I am flying\n" +
+              "I am singing\n" +
+              "I am Bird");
+  }
+}
---
+++ src/Module3/homework/Main.java
@@ -0,0 +1,8 @@
+package Module3.homework;
+
+public class Main {
+    public static void main(String[] args) {
+Bird cuckoo = new Bird();
+cuckoo.sing();
+    }
+}
